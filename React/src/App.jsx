a6774d16c69eaf39f685cb9eb213cb4776199aff
Mohammed Fayiz <fayizfaz78@gmail.com>
2023-07-06 00:02:33 +0530

    react router

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,13 +1,40 @@
-
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 import Home from "./pages/Home";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
 const App = () => {
-    return <Home />
-  };
-  
-  export default App
+  const user = true
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">
+        {user ? <Redirect to="/"/>: <Login/>}
+        </Route>
+        <Route path="/register">
+        {user ? <Redirect to="/"/>: <Register />}       
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
