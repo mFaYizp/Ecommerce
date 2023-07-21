03233ea05f7e1c7a894e41671c966f456aac9b22
Mohammed Fayiz <fayizfaz78@gmail.com>
2023-07-21 21:36:07 +0530

    use selector

--- React/src/components/Navbar.jsx
+++ React/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import SearchIcon from "@mui/icons-material/Search";
 import Badge from "@mui/material/Badge";
 import { ShoppingCartOutlined } from "@mui/icons-material";
 import { mobile } from "../responsive";
-import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   height: 60px;
@@ -90,7 +90,7 @@ const Navbar = () => {
             <MenuItem>SIGN IN</MenuItem>
             <Link to="/cart">
               <MenuItem>
-                <Badge badgeContent={quantity} color="secondary">
+                <Badge badgeContent={quantity} color="primary">
                   <ShoppingCartOutlined />
                 </Badge>
               </MenuItem>
--- React/src/index.js
+++ React/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import Store from "./redux/store";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
