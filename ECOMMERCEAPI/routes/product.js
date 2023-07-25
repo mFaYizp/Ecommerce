6c66df2ecfe8b181532ff2ad67cef049fb183e71
Mohammed Fayiz <fayizfaz78@gmail.com>
2023-07-25 11:05:31 +0530

    fixed getting products

--- ECOMMERCEAPI/routes/product.js
+++ ECOMMERCEAPI/routes/product.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 
 //CREATE
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newProduct = new Product (req.body);
+  const newProduct = new Product(req.body);
 
   try {
     const savedProduct = await newProduct.save();
@@ -64,14 +64,14 @@ router.get("/", async (req, res) => {
     } else if (qCategory) {
       products = await Product.find({
         categories: {
-          $in: [qCategory],
+          $in: [qCategory]
         },
       });
     } else {
       products = await Product.find();
     }
 
-    res.status(200).json(users);
+    res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
   }
--- React/src/pages/Home.jsx
+++ React/src/pages/Home.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Announcement from '../components/Announcement'
-import Slider from '../components/Slider'
-import Categories from '../components/Categories'
-import Products from '../components/Products'
-import Newsletter from '../components/Newsletter'
-import Footer from '../components/Footer'
-
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
-    <Announcement />
+      <Announcement />
       <Navbar />
       <Slider />
       <Categories />
@@ -20,7 +18,7 @@ const Home = () => {
       <Newsletter />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
