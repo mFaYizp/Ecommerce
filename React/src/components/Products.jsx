e978efbbe85d8e2bd4ebd0c153c31217bc8d488e
mFaYizp <fayizfaz78@gmail.com>
2023-08-04 22:32:54 +0530

    port

--- React/src/components/Products.jsx
+++ React/src/components/Products.jsx
@@ -19,8 +19,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `http://localhost:4000/api/products?category=${cat}`
+            : "http://localhost:4000/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
