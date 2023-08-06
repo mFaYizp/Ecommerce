53269d88b98b84a0b634b0105783507c8062cd0f
mFaYizp <fayizfaz78@gmail.com>
2023-08-06 13:18:00 +0530

    redux toolkit auth(Login)

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,22 +4,28 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
-import { BrowserRouter , Route, Routes , Navigate} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = true;
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/products/:category" element={<ProductList /> } />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
         <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />}/>
-        <Route path="/success" element={<Success />}/>
-        <Route path="/login" element={user ? (<Navigate replace to="/" /> ): (<Login />)}/>
-        <Route path="/register" element={user ?( <Navigate replace to="/" /> ):( <Register />)} />
-        
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/success" element={<Success />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to="/" /> : <Register />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- React/src/pages/Login.jsx
+++ React/src/pages/Login.jsx
@@ -1,5 +1,8 @@
-import  styled  from "styled-components"
+import styled from "styled-components";
 import { mobile } from "../responsive";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/apiCalls";
 
 const Container = styled.div`
   width: 100vw;
@@ -20,7 +23,7 @@ const Wrapper = styled.div`
   width: 25%;
   padding: 20px;
   background-color: white;
-  ${mobile({width: "75%" })}
+  ${mobile({ width: "75%" })}
 `;
 
 const Title = styled.h1`
@@ -36,7 +39,7 @@ const Form = styled.form`
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
-  margin:  10px 0px;
+  margin: 10px 0px;
   padding: 10px;
 `;
 
@@ -48,6 +51,14 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
+`;
+
+const Error = styled.span`
+  color: red;
 `;
 
 const Link = styled.a`
@@ -58,20 +69,39 @@ const Link = styled.a`
 `;
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Container>
-       <Wrapper>
+      <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
+          <Input
+            placeholder="username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
+          {error && <Error>Something went wrong...</Error>}
           <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
